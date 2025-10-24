a532af44b019faa0105c94b3bb48e3a3d86a965c
MarcusEduard <133214166+MarcusEduard@users.noreply.github.com>
2025-10-24 12:03:35 +0200

    Fix comments

--- WebAPI/Models/SurveyResponse.cs
+++ WebAPI/Models/SurveyResponse.cs
@@ -4,15 +4,15 @@ namespace WebAPI.Models
 {
     public class SurveyResponse
     {
-        public int SurveyResponseId { get; set; }       // Database primary key
-        public string SurveyId { get; set; }            // Unique survey ID (GUID)
-        public string CustomerName { get; set; }        // Previously CompanyName
-        public string Sector { get; set; }
-        public string CompanySize { get; set; }         // Micro, SME, Large
-        public string ProjectName { get; set; }
-        public string ProjectDuration { get; set; }
-        public DateTime? DateSent { get; set; }         // When survey was sent
-        public DateTime? DateCompleted { get; set; }    // When survey was completed
+        public int SurveyResponseId { get; set; }       // Primary key
+        public string SurveyId { get; set; }            // Unique survey ID
+        public string CustomerName { get; set; }        // CustomerName
+        public string Sector { get; set; }              // Sector
+        public string CompanySize { get; set; }         // CompanySize  
+        public string ProjectName { get; set; }         // ProjectName
+        public string ProjectDuration { get; set; }     // ProjectDuration
+        public DateTime? DateSent { get; set; }         // When survey was sent by admin
+        public DateTime? DateCompleted { get; set; }    // When survey was completed by the customer
         public int Question1 { get; set; }  // Overall satisfaction
         public int Question2 { get; set; }  // Professionalism
         public int Question3 { get; set; }  // International growth
@@ -21,10 +21,10 @@ namespace WebAPI.Models
         public int Question6 { get; set; }  // Communication
         public int Question7 { get; set; }  // Timeliness
         public int Question8 { get; set; }  // Advisor expertise
-        public int Question9 { get; set; }  // Future use
-        public DateTime SubmittedAt { get; set; }       // Legacy field - same as DateCompleted
+        public int Question9 { get; set; }  // Future use of TCH
+        public DateTime SubmittedAt { get; set; }       // DateCompleted
         
-        // Constructor to auto-generate SurveyId
+        // Auto-generating SurveyId
         public SurveyResponse()
         {
             SurveyId = Guid.NewGuid().ToString();
